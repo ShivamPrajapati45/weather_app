315d0773678810d11417b85761a07bf5293f101b
CodeWithShivam45 <prajapatishivam55296@gmail.com>
2025-04-09 17:52:31 +0530

    final push

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -2,6 +2,19 @@ import {MapContainer,TileLayer,Marker,Popup} from 'react-leaflet'
 import 'leaflet/dist/leaflet.css';
 import { WeatherData } from '@/api/types';
 import {LatLngExpression} from 'leaflet'
+import L from 'leaflet';
+
+// delete L.Icon.Default.prototype;
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl:
+        'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png',
+    iconUrl:
+        'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png',
+    shadowUrl:
+        'https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png',
+});
 
 interface Map{
     data: WeatherData
